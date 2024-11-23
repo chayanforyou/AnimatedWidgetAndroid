3d65b20a02e3ce7f4b8b271fc6eb1c80f798fc55
Chayan Mistry <chayanmistrry@gmail.com>
2024-11-23 10:13:02 +0600

    fix widget not updating issue after reboot the system

--- app/src/main/java/io/github/chayanforyou/animatedwidget/widget/AppWidgetProviderInfo.kt
+++ app/src/main/java/io/github/chayanforyou/animatedwidget/widget/AppWidgetProviderInfo.kt
@@ -11,45 +11,27 @@ import io.github.chayanforyou.animatedwidget.R
 
 class AppWidgetProviderInfo : AppWidgetProvider() {
 
-    companion object {
-        private const val PREFS_NAME = "widget_prefs"
-        private const val KEY_WIDGET_IDS = "existing_widget_ids"
-    }
-
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         super.onUpdate(context, appWidgetManager, appWidgetIds)
-
-        val sharedPrefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
-        val existingWidgetIds = sharedPrefs.getStringSet(KEY_WIDGET_IDS, emptySet())
-            ?.map { it.toInt() }?.toMutableSet() ?: mutableSetOf()
 
         val thisWidget = ComponentName(context, AppWidgetProviderInfo::class.java)
         val allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget)
 
         allWidgetIds.forEach { appWidgetId ->
-            if (!existingWidgetIds.contains(appWidgetId)) {
-                onNewWidgetCreated(context, appWidgetId)
-                existingWidgetIds.add(appWidgetId)
-            }
             updateAppWidget(context, appWidgetManager, appWidgetId)
         }
-
-        sharedPrefs.edit().putStringSet(KEY_WIDGET_IDS, existingWidgetIds.map { it.toString() }.toSet()).apply()
     }
 
     private fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
-       val onClickPendingIntent = createPendingIntent(context, appWidgetId,  AppWidgetReceiver.WIDGET_CLICK)
+        val onCreatePendingIntent = createPendingIntent(context, appWidgetId, AppWidgetReceiver.NEW_WIDGET)
+        onCreatePendingIntent.send()
 
+        val onClickPendingIntent = createPendingIntent(context, appWidgetId, AppWidgetReceiver.WIDGET_CLICK)
         val views = RemoteViews(context.packageName, R.layout.widget_layout).apply {
             setOnClickPendingIntent(R.id.widget, onClickPendingIntent)
         }
 
         appWidgetManager.updateAppWidget(appWidgetId, views)
-    }
-
-    private fun onNewWidgetCreated(context: Context, appWidgetId: Int) {
-        val onCreatePendingIntent = createPendingIntent(context, appWidgetId,  AppWidgetReceiver.NEW_WIDGET)
-        onCreatePendingIntent.send()
     }
 
     private fun createPendingIntent(context: Context, appWidgetId: Int, action: String): PendingIntent {
